c8208c7a068fcdf5130d4fb86f4780964674f465
di3go0-0 <d.rincon@utp.edu.co>
2025-03-19 22:56:10 -0500

    centros

--- RBF/src/helpers/centers.rs
+++ RBF/src/helpers/centers.rs
@@ -1,1 +1,9 @@
 // Calculo de los centros (x1, x2)
+
+pub fn get_center(x1: &Vec<i32>, x2: &Vec<i32>, i: usize) -> Option<Vec<i32>> {
+    if i < x1.len() && i < x2.len() {
+        Some(vec![x1[i], x2[i]])
+    } else {
+        None
+    }
+}
--- RBF/src/helpers/mod.rs
+++ RBF/src/helpers/mod.rs
@@ -1,1 +1,2 @@
+pub mod centers;
 pub mod utils;
