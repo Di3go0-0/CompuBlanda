43350f7b532dd458e7e61f49867353e565dbfe1a
di3go0-0 <d.rincon@utp.edu.co>
2025-03-21 12:18:29 -0500

    example

--- RBF/src/main.rs
+++ RBF/src/main.rs
@@ -1,10 +1,26 @@
+use helpers::{clasification::clasification, solver::weight_vector};
+
 mod helpers;
 
 fn main() {
-    let x1 = vec![0.0, 0.0, 1.0, 1.0];
-    let x2 = vec![0.0, 1.0, 0.0, 1.0];
-    let f = vec![0.0, 1.0, 1.0, 2.0];
+    let x1 = vec![1.0, 4.0, 2.0, 3.0, 2.0];
+    let x2 = vec![0.0, 1.0, 0.0, 1.0, 2.0];
+    let f = vec![1.0, 5.0, 2.0, 4.0, 4.0];
 
-    let weight_vector = helpers::solver::weight_vector(&x1, &x2, &f);
-    println!("vector: {:?}", weight_vector);
+    let weight_vector = weight_vector(&x1, &x2, &f);
+    // println!("vector: {:?}", weight_vector);
+
+    let p = vec![1.0, 1.5];
+    println!("pedicction data: {:?}", weight_vector);
+
+    let predicction = clasification(&weight_vector, &p, &x1, &x2);
+
+    println!("predicction: {}", predicction);
+
+    // panic!("Los vectores deben tener la misma longitud y no estar vacíos");
+    // let x1 = vec![0.0, 0.0, 1.0, 1.0];
+    // let x2 = vec![0.0, 1.0, 0.0, 1.0];
+    //
+    // let g = helpers::gaussiana::sigma::calculate_sigma(&x1, &x2).unwrap();
+    // println!("sigma: {}", g);
 }
